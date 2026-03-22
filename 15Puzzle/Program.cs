f3dcbfe63acd48779ebd6b929f88fa02d0687ded
BearFulmer <bearfulmer@gmail.com>
2026-03-22 08:10:00 -0700

    Add project files.

---
+++ 15Puzzle/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _15Puzzle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "15 puzzle";
+            Console.WriteLine("Welcome to 15 puzzle.");
+        }
+    }
+}
